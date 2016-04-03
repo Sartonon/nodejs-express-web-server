2ff9c7eef56f6f0738895a585e47085b9be3a8a8
Santeri Rusila <sajukaru@student.jyu.fi>
2016-04-03 15:36:50 +0300

    Removed empty line

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var middleware = {
 	},
 	logger: function(req, res, next) {
 		console.log("Request: " + " " + new Date().toString() + " " + req.method + " " + req.originalUrl);
-
 		next();
 	}
 };
